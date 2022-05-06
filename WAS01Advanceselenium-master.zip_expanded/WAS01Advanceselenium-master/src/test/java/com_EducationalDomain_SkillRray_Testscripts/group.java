3eb8a9cc8262f0d1ebfa5d14e75eda4d485c01ba
nandh <nandh@DESKTOP-LCQC60A>
2022-05-06 19:35:06 +0530

    group test case commited

---
+++ WAS01Advanceselenium-master.zip_expanded/WAS01Advanceselenium-master/src/test/java/com_EducationalDomain_SkillRray_Testscripts/group.java
@@ -0,0 +1,15 @@
+public class group
+{
+	public void tc1()(group = odd) {
+		
+	}
+public void tc2()(group = even) {
+		
+	}
+public void tc3() (group = odd){
+	
+}
+public void tc4() (group = even){
+	
+}
+}
